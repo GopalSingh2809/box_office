08484105f46ddafa00ffb37b940a1baa40deb737
Gopal <gss428860@gmail.com>
2023-08-01 08:13:52 -0800

    displaying shows and actor button on webpage

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -6,3 +6,5 @@ const apiGet = async queryString => {
   return body;
 };
 export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
+
+export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
